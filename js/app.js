bdc51399c82272cb29d5134bc38e5003c54cf111
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-06-07 15:28:57 -0300

    Seção 3: Javascript para QAs > 11. Operadores matemáticos

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ var userName = 'Fernando'
 
 document.getElementById('user-name').innerHTML = userName
 
+// Variáveis //
+
 // var nome = 'Mestre Yoda'
 // var idade = 100
 // var jedi = true
@@ -12,3 +14,18 @@ document.getElementById('user-name').innerHTML = userName
 // console.log(typeof idade)
 // console.log(typeof jedi)
 
+// Operadores matemáticos // 
+
+var n1 = 7
+var n2 = 2.5
+
+console.log(typeof n1)
+console.log(typeof n2)
+
+// + para somar
+// - para subtrair
+// * para multiplicar
+// / para dividir
+
+var total = n1 / n2
+console.log(total)
