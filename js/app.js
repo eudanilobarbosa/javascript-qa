76b729b2c6812b692c28d795d126f0a949c26e98
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-06-07 15:35:59 -0300

    Seção 3: Javascript para QAs > 12. Operadores de comparação

--- js/app.js
+++ js/app.js
@@ -16,16 +16,27 @@ document.getElementById('user-name').innerHTML = userName
 
 // Operadores matemáticos // 
 
-var n1 = 7
-var n2 = 2.5
+// var n1 = 7
+// var n2 = 2.5
 
-console.log(typeof n1)
-console.log(typeof n2)
+// console.log(typeof n1)
+// console.log(typeof n2)
 
 // + para somar
 // - para subtrair
 // * para multiplicar
 // / para dividir
 
-var total = n1 / n2
-console.log(total)
+// var total = n1 / n2
+// console.log(total)
+
+// Operadores de comparação //
+
+var v1 = 5
+var v2 = '5'
+
+var resultado = v1 !== v2
+console.log(resultado)
+
+
+
