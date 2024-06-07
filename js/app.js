c9dd4c6099abf29aebd034ae3c48a1afa573cc05
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-06-07 14:59:45 -0300

    Seção 3: Javascript para QAs > 10. Variáveis de tipos de dados

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 console.log('Olá, Javascript!')
+
+var userName = 'Fernando'
+
+document.getElementById('user-name').innerHTML = userName
+
+// var nome = 'Mestre Yoda'
+// var idade = 100
+// var jedi = true
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof jedi)
+
