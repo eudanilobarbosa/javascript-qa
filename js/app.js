e8931647db42df83f3cb6d623434c1278b99f87e
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-06-07 16:17:58 -0300

    Seção 3: Javascript para QAs > 14. Controle de Fluxos com BDD (Behavior-Driven Development)

--- js/app.js
+++ js/app.js
@@ -50,12 +50,52 @@ document.getElementById('user-name').innerHTML = userName
 //     alert(nome + ', Seja bem-vindo(a)')
 // }
 
-
 // boasVindas('Fernando')
 
-function soma(n1, n2) {
-    return n1 + n2
+// function soma(n1, n2) {
+//     return n1 + n2
+// }
+
+// var resultado = soma(5,5)
+// console.log(resultado)
+
+// Controle de fluxos //
+
+// Sendo um cliente correntista do banco
+// Posso sacar dinheiro em caixas eletrônicos
+// Para poder comprar em lugares que não aceitam o cartão de débito ou crédito
+
+var saldo = 1000
+
+function saque(valor) {
+
+    if (valor > saldo) {
+        console.log('Valor do saque superior ao saldo')
+    } else if (valor > 700) {
+        console.log('Valor do saque é superior ao máximo permitido por operação')
+    } else {
+        saldo = saldo - valor
+    }
+
 }
 
-var resultado = soma(5,5)
-console.log(resultado)
+saque(701)
+console.log(saldo)
+
+// Cenario 1: Saque com sucesso
+// Dado que meu saldo é de 100 reais
+// Quando faço um saque de 500 reais
+// Então o valor do saque deve ser deduzido do meu saldo
+
+// Cenario 2: Saque com valor superior ao saldo
+// Dado que meu saldo é de 1000 reais
+// Quando eu faço um saque de 1001 reais
+// Então não deve deduzir do meu saldo
+// E deve mostrar uma mensagem de alerta informando que o valor é superior ao saldo
+
+// Cenario 3: Saque com valor máximo
+// Dado que meu saldo é de 1000 reais
+// E o valor máximo por operação é de 700 reais
+// Quando faço um saque no valor de 701 reais
+// Então não deve deduzir do meu saldo
+// E deve mostar uma mensagem de alerta informando que o valor é superior ao máximo permitido por operação
