668417b87fcc1c2284918f7297b75b6c690058e2
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-06-10 08:53:29 -0300

    Seção 4: Construindo uma WebApp > 21. Implementando funções

--- js/app.js
+++ js/app.js
@@ -49,5 +49,10 @@ const App = new Vue({
         title: 'Star Wars Lego',
         userName: 'Fernando',
         characters: LIST
+    },
+    methods: {
+        like(userName) {
+            alert(`O personagem ${userName} recebeu um like!`)
+        }
     }
 })
