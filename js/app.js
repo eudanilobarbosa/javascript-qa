a1b384cda7710f2287ea3be6e659a564494af92d
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-06-10 08:47:28 -0300

    Seção 4: Construindo uma WebApp > 20. Renderização de elementos usando a diretiva v-for

--- js/app.js
+++ js/app.js
@@ -1,9 +1,53 @@
 
 
+const LIST = [
+    {
+        id: 1,
+        nome: 'Mestre Yoda',
+        avatar: 'images/yoda.png'
+    },
+    {
+        id: 2,
+        nome: 'Luke Skywalker',
+        avatar: 'images/luke.png'
+    },
+    {
+        id: 3,
+        nome: 'Princesa Leia',
+        avatar: 'images/leia.png'
+    },
+    {
+        id: 4,
+        nome: 'Han Solo',
+        avatar: 'images/hansolo.png'
+    },
+    {
+        id: 5,
+        nome: 'Darth Vader',
+        avatar: 'images/vader.png'
+    },
+    {
+        id: 6,
+        nome: 'Chewbacca',
+        avatar: 'images/chewbacca.png'
+    },
+    {
+        id: 7,
+        nome: 'R2D2',
+        avatar: 'images/r2d2.png'
+    },
+    {
+        id: 8,
+        nome: 'C3pO',
+        avatar: 'images/c3po.png'
+    }
+]
+
 const App = new Vue({
     el: '#app',
     data: {
         title: 'Star Wars Lego',
-        userName: 'Fernando'
+        userName: 'Fernando',
+        characters: LIST
     }
 })
