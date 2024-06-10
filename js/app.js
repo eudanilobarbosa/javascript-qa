aa890199579be5578a8cc3f121dcb168709bca73
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-06-10 09:14:19 -0300

    Seção 4: Construindo uma WebApp > 22. Criando um filtro de busca

--- js/app.js
+++ js/app.js
@@ -48,11 +48,30 @@ const App = new Vue({
     data: {
         title: 'Star Wars Lego',
         userName: 'Fernando',
-        characters: LIST
+        characters: LIST,
+        searchName: ''
     },
     methods: {
         like(userName) {
             alert(`O personagem ${userName} recebeu um like!`)
+        },
+        search() {
+
+            if (this.searchName === '') {
+                return alert('O campo de busca é obrigatório')
+            }
+
+            const list = this.characters = LIST
+
+            const result = list.filter((item) => {
+                return item.nome === this.searchName
+            })
+
+            if (result.length <= 0) {
+                alert('Nenhum registro encontrado')
+            } else {
+                this.characters = result
+            }
         }
     }
 })
